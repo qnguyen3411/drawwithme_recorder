eab1667fc70d31ff3e07a3e76b5bf325b355c4c9
Quang Nguyen <qnguyen3411@gmail.com>
2018-11-03 13:14:28 -0700

    moved static server to same folder

--- server.js
+++ server.js
@@ -1,108 +1,61 @@
-const { promisify } = require('util');
 const fs = require('fs');
-const redis = require("redis")
-const server = require('http').createServer();
+const { promisify } = require('util');
+
+const express = require('express');
+const compression = require('compression')
+const redis = require("redis");
 
 const BATCH_SIZE = 100;
 
 const redisClient = redis.createClient();
-const getAsync = promisify(redisClient.get).bind(redisClient);
-const setAsync = promisify(redisClient.set).bind(redisClient);
-const incrAsync = promisify(redisClient.incr).bind(redisClient);
-
 redisClient.on("error", function (err) {
   console.log("Error " + err);
 });
+// const getAsync = promisify(redisClient.get).bind(redisClient);
 
 
-let writeStreams = {};
-
-var io = require('socket.io')(server);
-io.on('connection', function (client) {
-  console.log("CLIENT CONNECTED")
-
-  client.on('write', async function ({ roomId, data }) {
-    console.log(roomId);
-    try {
-      if (!writeStreams[roomId]) {
-        // Check redis for room
-        const status = await getAsync(`${roomId}_status`);
-        if (status === null) {
-          await initializeRoom(roomId)
-          writeStreams[roomId] = await createNewWriteStream(roomId, 0);
-        } else if (status === 'active') {
-          const strokeCount = await getAsync(`${roomId}_strokeCount`);
-          const batchNum = Math.floor(strokeCount / BATCH_SIZE);
-          writeStreams[roomId] = await openWriteStream(roomId, batchNum);
-        } else {
-          throw new Error("ROOM IS INACTIVE");
-        }
-      }
-
-      writeStreams[roomId].write(',\n' + JSON.stringify(data));
-      const count = incrAsync(`${roomId}_strokeCount`);
-      if (count % BATCH_SIZE === 0) {
-        const batchNum = Math.round(count / BATCH_SIZE);
-        writeStreams[roomId] = await createNewWriteStream(roomId, batchNum);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-
-  });
-
-  async function initializeRoom(roomId) {
-    try {
-      await redisClient.set(`${roomId}_status`, 'active');
-      await redisClient.set(`${roomId}_strokeCount`, '0');
-      return;
-    } catch (err) {
-      throw err;
-    }
-  }
-
-  async function createNewWriteStream(roomId, batchNum) {
-    try {
-      const stream = await openWriteStream(roomId, batchNum);
-      stream.write("[{'rgba':[1,1,1,1], 'size': 1, x: [], y: []}");
-      return stream;
-    } catch (err) {
-      throw err;
-    }
-  }
-
-  async function openWriteStream(roomId, batchNum) {
-    return new Promise((resolve, reject) => {
-      const strokeLogPath = getFileName(roomId, batchNum);
-      try {
-        const stream = fs.createWriteStream(strokeLogPath, { flags: 'a', mode: 777 });
-        stream.on('open', () => {
-          fs.chmodSync(strokeLogPath, '755');
-          while(!stream.writable) { /* Wait til stream is writable */ }
-          resolve(stream);
-        });
-        
-
-      } catch (err) {
-        reject(err);
-      };
+const app = express();
+app.use(compression());
+app.use(express.static('public'))
+app.listen(1337);
 
 
+// app.get('/log/latest/:roomId', async (req, res) => {
+  
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   res.setHeader('Access-Control-Allow-Origin', '*');
 
-    });
-  }
+//   const { roomId } = req.params;
+//   const strokeCount = await getAsync(`${roomId}_strokeCount`);
+//   const maxBatchNum = Math.floor(strokeCount / BATCH_SIZE);
+//   let s = fs.createReadStream(__dirname + `/../strokeLogs/${roomId}_${maxBatchNum}.txt`)
+//     .pipe(res)
+//     .on('error', function (err) {
+//       console.log('Error while reading file.', err);
+//       res.status(500).send('Server error');
+//     })
+//     .on('end', function () {
+//       console.log('Read entire file.')
+//     });
 
-  function getFileName(roomId, batchNum) {
-    return __dirname + `/../strokeLogs/${roomId}_${batchNum}.txt`
-  }
+// })
 
-  client.on('snapshot', async function ({ roomId, data }) {
+// app.get('/log/:batchNum/:roomId', async (req, res) => {
+//   const { roomId, batchNum } = req.params;
+//   const strokeCount = getAsync(`${roomId}_strokeCount`);
+//   const maxBatchNum = Math.floor(strokeCount / BATCH_SIZE);
+//   if (batchNum > maxBatchNum) {
+//     res.send("");
+//   }
+//   let s = fs.createReadStream(__dirname + `/../strokeLogs/${roomId}_${batchNum}.txt`)
+//     .pipe(res)
+//     .on('error', function (err) {
+//       console.log('Error while reading file.', err);
+//       res.status(500).send('Server error');
 
-  });
+//     })
+//     .on('end', function () {
+//       console.log('Read entire file.')
+//     });
+// })
 
-  client.on('end', function ({ roomId }) {
-
-  }),
-    client.on('disconnect', function () { });
-});
-server.listen(9000);
