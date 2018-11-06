eb6d056dc37b97e81fe16d65f7640bb2d369a756
Quang Nguyen <qnguyen3411@gmail.com>
2018-11-05 17:24:43 -0800

    switched from socket to http

--- drawalong.js
+++ drawalong.js
@@ -1,7 +1,17 @@
-const { createCanvas, loadImage } = require('canvas')
+// const { createCanvas, loadImage } = require('canvas')
+
+const fs = require('fs');
+// const canvas = createCanvas(1000, 1000, 'png');
+// const ctx = canvas.getContext('2d');
+
+// console.log(canvas, ctx);
+fs.writeFile("test.txt", "hhey", { flag: 'a', mode: 33279 }, (err) => {
+  if (err) { console.log(err); }
+})
 
 
-const canvas = createCanvas(1000, 1000, 'png');
-const ctx = canvas.getContext('2d');
-
-console.log(canvas, ctx);
+for(let i = 0; i < 10000; i++) {
+  fs.writeFile("test.txt", i+ "\n", { flag: 'a', mode: 33279 }, function (err) {
+      if (err) console.log(err);
+  })
+}
--- recorder2.js
+++ recorder2.js
@@ -1,11 +1,15 @@
 const { promisify } = require('util');
+const express = require('express');
+const bodyParser = require('body-parser');
 const fs = require('fs');
 const redis = require("redis")
 const { createCanvas, loadImage } = require('canvas')
 
-const server = require('http').createServer();
-server.listen(9000);
+const app = express();
 
+app.use(bodyParser.json());
+app.listen(9000);
+console.log("PID: ", process.pid);
 
 const BATCH_SIZE = 100;
 
@@ -21,171 +25,174 @@ const incrAsync = promisify(redisClient.incr).bind(redisClient);
 let buffers = {};
 
 
-var io = require('socket.io')(server);
 
-io.on('connection', function (client) {
-
-  console.log("CONNECTED WITH SOCKET SERVER")
-  setInterval(unloadBuffers, 20000);
-
-  client.on('write', async function ({ roomId, data }) {
-    try {
-      if (buffers[roomId] === undefined) {
-        const status = await getAsync(`${roomId}_status`);
-        if (status === 'inactive') { return; }
-        if (status === null) { // If brand new room
-          await setAsync(`${roomId}_strokeCount`, '0');
-          setAsync(`${roomId}_status`, 'active');
-          initializeLog(roomId);
-          initializeSnapshot(roomId)
-        }
-        buffers[roomId] = []
+app.post('/write/:roomId', async (req, res) => {
+  const { data } = req.body;
+  const { roomId } = req.params;
+  res.status(200).send("")
+  try {
+    if (buffers[roomId] === undefined) {
+      const status = await getAsync(`${roomId}_status`);
+      if (status === 'inactive') { return; }
+      if (status === null) { // If brand new room
+        await setAsync(`${roomId}_strokeCount`, '0');
+        setAsync(`${roomId}_status`, 'active');
+        initializeLog(roomId);
+        initializeSnapshot(roomId)
       }
-
-      buffers[roomId].push(data);
-      incrAsync(`${roomId}_strokeCount`);
-
-    } catch (err) {
-      console.error(err);
+      buffers[roomId] = []
     }
-  });
 
-  client.on('snapShot', async function ({ roomId }) {
-    recordBuffer(roomId, buffers[roomId])
-  });
+    buffers[roomId].push(data);
+    incrAsync(`${roomId}_strokeCount`);
 
-  client.on('end', function ({ roomId }) {
-    recordBuffer(roomId, buffers[roomId])
-    setAsync(`${roomId}_status`, 'inactive');
+  } catch (err) {
+    console.error(err);
+  }
+})
 
-    if (buffers[roomId]) {
-      delete buffers[roomId];
+app.post('/snapshot/:roomId', async (req, res) => {
+  recordBuffer(roomId, buffers[roomId])
+  res.status(200).send("")
+})
+
+app.post('/end/:roomId', async (req, res) => {
+  recordBuffer(roomId, buffers[roomId])
+  setAsync(`${roomId}_status`, 'inactive');
+
+  if (buffers[roomId]) {
+    delete buffers[roomId];
+  }
+  res.status(200).send("")
+
+})
+
+
+setInterval(unloadBuffers, 20000);
+
+function unloadBuffers() {
+  console.log("UNLOADED")
+  Object.entries(buffers).forEach(async ([roomId, buffer]) => {
+    console.log(buffers)
+    if (!buffer || buffer.length === 0) { return; }
+    await recordBuffer(roomId, buffer);
+    while (buffer.length !== 0) {
+      buffer.pop()
     }
-  });
+  })
+}
 
-  client.on('disconnect', function () { });
-
-  function unloadBuffers() {
-    console.log("UNLOADING BUFFERS...")
-    Object.entries(buffers).forEach(([roomId, buffer]) => {
-      if (buffer && buffer.length === 0) { return; }
-      recordBuffer(roomId, buffer);
-      buffers[roomId] = [];
-    })
-  }
-
-  async function recordBuffer(roomId, strokeBuffer) {
-    try {
-      const ctx = await getSnapshotCtx(roomId);
-      let logStr = "";
-      for (let i = 0; i < strokeBuffer.length; i++) {
-        draw(ctx, strokeBuffer[i]);
-        logStr += ',\n' + JSON.stringify(strokeBuffer[i])
-      }
-      snapShot(roomId, ctx.canvas);
-      writeToLog(roomId, logStr)
-    } catch (err) {
-      console.log(err)
+async function recordBuffer(roomId, strokeBuffer) {
+  try {
+    const ctx = await getSnapshotCtx(roomId);
+    let logStr = "";
+    for (let i = 0; i < strokeBuffer.length; i++) {
+      draw(ctx, strokeBuffer[i]);
+      logStr += ',\n' + JSON.stringify(strokeBuffer[i])
     }
+
+    snapShot(roomId, ctx.canvas);
+    writeToLog(roomId, logStr)
+  } catch (err) {
+    console.log(err)
   }
+}
 
-  async function getSnapshotCtx(roomId) {
-    try {
-      console.log("TRYING TO LOAD IMAGE")
-      const image = await loadImage(getSnapshotUrl(roomId));
-      const canvas = createCanvas(1820, 1024, 'png');
-      const ctx = canvas.getContext('2d');
-      ctx.drawImage(image, 0, 0);
-      return ctx;
-    } catch (err) {
-      throw err
-    }
-   
-  }
-
-  async function writeToLog(roomId, logStr) {
-    console.log("WRITING LOGSTR: ", logStr)
-    const strokeCount = await getAsync(`${roomId}_strokeCount`);
-    const batchNum = Math.floor(strokeCount / BATCH_SIZE);
-
-    const fileName = getFileName(roomId, batchNum);
-    fs.writeFile(fileName, logStr, { flag: 'a', mode: 33279 }, (err) => {
-      if (err) throw err;
-      console.log("WRITE SUCCESSFUL")
-    })
-  }
-
-
-  function getFileName(roomId, batchNum) {
-    return __dirname + `/public/logs/${roomId}_${batchNum}.txt`
-  }
-
-  function draw(ctx, stroke) {
-    const cssString = `rgba(${stroke.rgba.join(',')})`;
-    ctx.lineJoin = 'round';
-    ctx.lineCap = 'round';
-    if (stroke.x.length === 0) {
-      ctx.lineWidth = 1;
-      ctx.fillStyle = cssString;
-      drawDot(ctx, stroke.x, stroke.y);
-    } else {
-      ctx.lineWidth = stroke.size
-      ctx.strokeStyle = cssString;
-      drawPath(ctx, stroke.x, stroke.y);
-    }
-  }
-
-  function drawPath(ctx, x, y) {
-    ctx.moveTo(x[0], x[0]);
-    ctx.beginPath();
-    const len = x.length;
-    for (let i = 1; i < len; i++) {
-      ctx.lineTo(x[i], y[i]);
-    }
-    ctx.stroke();
-  }
-
-  function drawDot(ctx, x, y) {
-    ctx.beginPath();
-    ctx.arc(x[0], y[0], size / 2, 0, 2 * Math.PI);
-    ctx.fill();
-  }
-
-  function initializeLog(roomId) {
-    return new Promise((resolve, reject) => {
-      const initialStr = '[{"rgba":[1,1,1,1], "size": 1, "x": [], "y": []}';
-      const fileName = getFileName(roomId, 0);
-      fs.writeFile(fileName, initialStr, { flags: 'a', mode: 33279 }, (err) => {
-        if (err) { reject(err); }
-        resolve();
-      })
-    })
-  }
-
-  function getFileName(roomId, batchNum) {
-    return __dirname + `/public/logs/${roomId}_${batchNum}.txt`
-  }
-
-  async function initializeSnapshot(roomId) {
+async function getSnapshotCtx(roomId) {
+  try {
+    console.log("TRYING TO LOAD IMAGE")
+    const image = await loadImage(getSnapshotUrl(roomId));
     const canvas = createCanvas(1820, 1024, 'png');
-    await snapShot(roomId, canvas)
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(image, 0, 0);
+    return ctx;
+  } catch (err) {
+    throw err
   }
 
-  function snapShot(roomId, canvas) {
-    return new Promise((resolve, reject) => {
-      const out = fs.createWriteStream(
-        getSnapshotUrl(roomId),
-        { mode: 33279, flag: 'w' }
-      )
-      const stream = canvas.createPNGStream()
-      stream.pipe(out)
-      out.on('finish', () => resolve());
-      out.on('error', (err) => reject(err));
+}
+
+async function writeToLog(roomId, logStr) {
+  console.log("WRITING LOGSTR: ", logStr)
+  const strokeCount = await getAsync(`${roomId}_strokeCount`);
+  const batchNum = Math.floor(strokeCount / BATCH_SIZE);
+
+  const fileName = getFileName(roomId, batchNum);
+  fs.writeFile(fileName, logStr, { flag: 'a', mode: 33279 }, (err) => {
+    if (err) throw err;
+    console.log("WRITE SUCCESSFUL")
+  })
+}
+
+
+function getFileName(roomId, batchNum) {
+  return __dirname + `/public/logs/${roomId}_${batchNum}.txt`
+}
+
+function draw(ctx, stroke) {
+  const cssString = `rgba(${stroke.rgba.join(',')})`;
+  ctx.lineJoin = 'round';
+  ctx.lineCap = 'round';
+  if (stroke.x.length === 0) {
+    ctx.lineWidth = 1;
+    ctx.fillStyle = cssString;
+    drawDot(ctx, stroke.x, stroke.y);
+  } else {
+    ctx.lineWidth = stroke.size
+    ctx.strokeStyle = cssString;
+    drawPath(ctx, stroke.x, stroke.y);
+  }
+}
+
+function drawPath(ctx, x, y) {
+  ctx.moveTo(x[0], x[0]);
+  ctx.beginPath();
+  const len = x.length;
+  for (let i = 1; i < len; i++) {
+    ctx.lineTo(x[i], y[i]);
+  }
+  ctx.stroke();
+}
+
+function drawDot(ctx, x, y) {
+  ctx.beginPath();
+  ctx.arc(x[0], y[0], size / 2, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+function initializeLog(roomId) {
+  return new Promise((resolve, reject) => {
+    const initialStr = '[{"rgba":[1,1,1,1], "size": 1, "x": [], "y": []}';
+    const fileName = getFileName(roomId, 0);
+    fs.writeFile(fileName, initialStr, { flags: 'a', mode: 33279 }, (err) => {
+      if (err) { reject(err); }
+      resolve();
     })
-  }
+  })
+}
 
-  function getSnapshotUrl(roomId) {
-    return __dirname + `/public/snapShots/${roomId}_snapshot.png`;
-  }
+function getFileName(roomId, batchNum) {
+  return __dirname + `/public/logs/${roomId}_${batchNum}.txt`
+}
 
-});
+async function initializeSnapshot(roomId) {
+  const canvas = createCanvas(1820, 1024, 'png');
+  await snapShot(roomId, canvas)
+}
+
+function snapShot(roomId, canvas) {
+  return new Promise((resolve, reject) => {
+    const out = fs.createWriteStream(
+      getSnapshotUrl(roomId),
+      { mode: 33279, flag: 'w' }
+    )
+    const stream = canvas.createPNGStream()
+    stream.pipe(out)
+    out.on('finish', () => resolve());
+    out.on('error', (err) => reject(err));
+  })
+}
+
+function getSnapshotUrl(roomId) {
+  return __dirname + `/public/snapShots/${roomId}_snapshot.png`;
+}
